8fa06b869bb7706e34184da4e9d31df3f6e8e40b
SadeEtensh <eteneshsade550305@gmail.com>
2022-08-21 14:51:35 +0100

    Added permission functionality

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,4 +17,8 @@ from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/v1/", include("posts.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("posts.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
+    pagination_class = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    # permission_classes = (permissions.IsAdminUser,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
